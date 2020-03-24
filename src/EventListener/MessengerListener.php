5e9f5f2496f25740b03181fad6e32aaa27fd0b70
shuba.ivan <i.shuba@kf.ua>
2020-03-24 16:02:23 +0200

    logs

--- src/EventListener/MessengerListener.php
+++ src/EventListener/MessengerListener.php
@@ -29,33 +29,39 @@ class MessengerListener implements EventSubscriberInterface
 
     public function onSendToTransport(SendMessageToTransportsEvent $event)
     {
-        $this->getLogger()->debug('Message dispatched');
+        $this->getLogger()->debug('Message dispatched: '
+            . (get_class($event->getEnvelope()->getMessage()) ?? null));
     }
 
     public function onFailed(WorkerMessageFailedEvent $event)
     {
         if ($event->getThrowable()) {
-            $this->getLogger()->error($event->getThrowable()->getMessage());
+            $this->getLogger()->error('Message failed'
+                . $event->getThrowable()->getMessage());
         }
-        $this->getLogger()->debug('Message failed');
 
         if ($event->willRetry()) {
-            $this->getLogger()->debug('Message will be retried');
+            $this->getLogger()->debug('Message will be retried'
+                . ($event->getThrowable()->getMessage() ?? null));
         } else {
-            $this->getLogger()->debug('Message will not be retried');
+            $this->getLogger()->debug('Message will not be retried'
+                . ($event->getThrowable()->getMessage() ?? null));
         }
     }
 
     public function onReceived(WorkerMessageReceivedEvent $event)
     {
         if ($event->getEnvelope()) {
-            $this->getLogger()->debug('Received message ' . get_class($event->getEnvelope()->getMessage()));
+            $this->getLogger()->debug('Received message '
+                . (get_class($event->getEnvelope()->getMessage()) ?? null));
         }
     }
 
     public function onHandled(WorkerMessageHandledEvent $event)
     {
-        $this->getLogger()->debug('Task handled');
+        $this->getLogger()->debug('Task handled by receiver name'
+            . $event->getReceiverName() . ' message: '
+            . (get_class($event->getEnvelope()->getMessage() ?? null)));
     }
 
     public static function getSubscribedEvents()
