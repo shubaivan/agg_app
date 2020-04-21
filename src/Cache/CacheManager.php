2277f443a0375c4c144f1d8761c7471ca748f680
shuba.ivan <i.shuba@kf.ua>
2020-04-21 10:59:04 +0300

    add category and shop to clear cache

--- src/Cache/CacheManager.php
+++ src/Cache/CacheManager.php
@@ -19,6 +19,16 @@ class CacheManager
     private $pdo_product_cache_pool;
 
     /**
+     * @var TagAwareAdapter
+     */
+    private $pdo_shop_cache_pool;
+
+    /**
+     * @var TagAwareAdapter
+     */
+    private $pdo_category_cache_pool;
+
+    /**
      * @var TraceableAdapter
      */
     private $doctrine_result_cache_pool;
@@ -32,13 +42,23 @@ class CacheManager
      * CacheManager constructor.
      * @param TagAwareAdapter $pdo_common_cache_pool
      * @param TagAwareAdapter $pdo_product_cache_pool
+     * @param TagAwareAdapter $pdo_shop_cache_pool
+     * @param TagAwareAdapter $pdo_category_cache_pool
      * @param TraceableAdapter $doctrine_result_cache_pool
      * @param TraceableAdapter $doctrine_system_cache_pool
      */
-    public function __construct(TagAwareAdapter $pdo_common_cache_pool, TagAwareAdapter $pdo_product_cache_pool, TraceableAdapter $doctrine_result_cache_pool, TraceableAdapter $doctrine_system_cache_pool)
+    public function __construct(
+        TagAwareAdapter $pdo_common_cache_pool,
+        TagAwareAdapter $pdo_product_cache_pool,
+        TagAwareAdapter $pdo_shop_cache_pool,
+        TagAwareAdapter $pdo_category_cache_pool,
+        TraceableAdapter $doctrine_result_cache_pool,
+        TraceableAdapter $doctrine_system_cache_pool)
     {
         $this->pdo_common_cache_pool = $pdo_common_cache_pool;
         $this->pdo_product_cache_pool = $pdo_product_cache_pool;
+        $this->pdo_shop_cache_pool = $pdo_shop_cache_pool;
+        $this->pdo_category_cache_pool = $pdo_category_cache_pool;
         $this->doctrine_result_cache_pool = $doctrine_result_cache_pool;
         $this->doctrine_system_cache_pool = $doctrine_system_cache_pool;
     }
@@ -49,5 +69,7 @@ class CacheManager
         $this->doctrine_system_cache_pool->clear();
         $this->pdo_product_cache_pool->clear();
         $this->pdo_common_cache_pool->clear();
+        $this->pdo_shop_cache_pool->clear();
+        $this->pdo_category_cache_pool->clear();
     }
 }
