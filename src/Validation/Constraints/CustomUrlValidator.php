b2f881da70477c3e6afce82ab70fb7a709b0dcb6
shuba.ivan <i.shuba@kf.ua>
2020-07-25 13:28:54 +0300

    modify regex for custom validation url

--- src/Validation/Constraints/CustomUrlValidator.php
+++ src/Validation/Constraints/CustomUrlValidator.php
@@ -21,8 +21,8 @@ class CustomUrlValidator extends UrlValidator
                 \]  # an IPv6 address
             )
             (:[0-9]+)?                              # a port (optional)
-            (?:/ (?:[\pL\pN\-._\~!$&\'()*+™®,;=:@]|%%[0-9A-Fa-f]{2})* )*          # a path
-            (?:\? (?:[\pL\pN\-._\~!$&\'\[\]()*+™®,;=:@/?]|%%[0-9A-Fa-f]{2})* )?   # a query (optional)
-            (?:\# (?:[\pL\pN\-._\~!$&\'()*+™®,;=:@/?]|%%[0-9A-Fa-f]{2})* )?       # a fragment (optional)
+            (?:/ (?:[\pL\pN\-._\~!$&\'()*+™® ,;=:@]|%%[0-9A-Fa-f]{2})* )*          # a path
+            (?:\? (?:[\pL\pN\-._\~!$&\'\[\]()*+™® ,;=:@/?]|%%[0-9A-Fa-f]{2})* )?   # a query (optional)
+            (?:\# (?:[\pL\pN\-._\~!$&\'()*+™® ,;=:@/?]|%%[0-9A-Fa-f]{2})* )?       # a fragment (optional)
         $~ixu';
 }
