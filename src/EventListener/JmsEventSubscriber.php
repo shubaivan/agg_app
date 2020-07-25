26d5b29409b00565bc5ecde10e84adc757405658
shuba.ivan <i.shuba@kf.ua>
2020-07-25 10:17:27 +0300

    post deserializer set group identity

--- src/EventListener/JmsEventSubscriber.php
+++ src/EventListener/JmsEventSubscriber.php
@@ -5,6 +5,7 @@ namespace App\EventListener;
 
 use App\Entity\Product;
 use JMS\Serializer\EventDispatcher\EventSubscriberInterface;
+use JMS\Serializer\EventDispatcher\ObjectEvent;
 use JMS\Serializer\EventDispatcher\PreDeserializeEvent;
 
 class JmsEventSubscriber implements EventSubscriberInterface
@@ -16,8 +17,22 @@ class JmsEventSubscriber implements EventSubscriberInterface
                 'event' => 'serializer.pre_deserialize',
                 'class' => Product::class,
                 'method' => 'onPreDeserializeProduct',
+            ),
+            array(
+                'event' => 'serializer.post_deserialize',
+                'class' => Product::class,
+                'method' => 'onPostDeserializeProduct',
             )
         );
+    }
+
+    public function onPostDeserializeProduct(ObjectEvent $event)
+    {
+        /** @var Product $object */
+        $object = $event->getObject();
+        if (!$object->getGroupIdentity()) {
+            $object->setGroupIdentity($object->getSku());
+        }
     }
 
     /**
