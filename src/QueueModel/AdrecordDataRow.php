f3ac038137c25f4703d8277701d10379c730356c
shuba.ivan <shuba.ivan@yandex.ru>
2020-09-07 18:42:00 +0300

    fix adrecord sku identity

--- src/QueueModel/AdrecordDataRow.php
+++ src/QueueModel/AdrecordDataRow.php
@@ -51,4 +51,13 @@ class AdrecordDataRow extends ResourceProductQueues
     {
         return $this->row['EAN'] ?? null;
     }
+
+    public function generateIdentityUniqData()
+    {
+        if (isset($this->row['identityUniqData']) && strlen($this->row['identityUniqData'])) {
+            return $this->row['identityUniqData'];
+        }
+
+        return $this->getSku();
+    }
 }
